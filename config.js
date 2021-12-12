2c2a33e0fc3df2690ea49435101764ff7cfd5f2b
thuykieu2k3 <95998563+thuykieu2k3@users.noreply.github.com>
2021-12-12 15:36:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     titleWeb: "QUÀ GIÁNG SINH",
-    introTitle: 'Nguyễn Thị Thúy Kiều',
+    introTitle: 'Nguyễn Thị Thúy Kiều(●'◡'●)',
     introDesc: `Trái đất vốn lạ thường.
     Mà sao em cứ đi nhầm đường.
     Lạc vào tim anh lẻ loi.
     Đằng sau chữ yêu đây là thương.`,
-    btnIntro: '^^HiHi^^',
-    title: 'Thúy Kiều mệt mỏi bơ phờ.🥰Nên đành nhẫn nhịn"thôi ơ yêu Anh"',
-    desc: 'Em yêu hay không yêu nói một lời!<3 ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    btnIntro: '^^HiHi^_^',
+    title: 'Phải chăng Anh đã yêu ngay từ cái nhìn đầu tiên😍😍😍.Phải chăng Anh đã say ngay từ lúc thấy nụ cười ấy💕💕💕',
+    desc: 'Yêu hay không yêu nói một lời!<3 ',
+    btnYes: '✔Thích lắm😊😊😊 ',
+    btnNo: '(❁´◡`❁)Không nha 😜😜😜',
     question: 'Em có biết vì sao trên tiktok nhiều cô gái xinh, nhưng anh lại chọn em để xem hông?',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <3333',
-    mess: 'Anh cũng không biết nữa 😘😘',
+    mess: 'Anh cũng không biết nữa😅😅😅',
     messDesc: 'Tối nay nhớ live tiktok 11h nha!.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
