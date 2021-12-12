60ea153e46ef6a197c22d37df69c6ce45599b436
thuykieu2k3 <95998563+thuykieu2k3@users.noreply.github.com>
2021-12-12 16:10:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Nguyễn Thị Thúy Kiều',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm ❤❤❤',
+    btnNo: 'Không nha 😂😂😂',
+    question: 'Em biế vì sao giữa nghìn cô gái xinh trên tiktok,nhưng anh vẫn chọn em để xem hông🙄🙄🙄',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3333',
+    mess: 'Anh cũng không biết nữa😁😁😁',
+    messDesc: 'Tôi nay nhớ live tiktok lúc 11h nhen.',
+    btnAccept: 'Okiiiii lun 😍',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
