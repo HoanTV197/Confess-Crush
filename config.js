c9bfcdf7512d43fc5589e853ce050fb2cc730918
thuykieu2k3 <95998563+thuykieu2k3@users.noreply.github.com>
2021-12-12 15:26:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
+    titleWeb: "QUÀ GIÁNG SINH",
     introTitle: 'Nguyễn Thị Thúy Kiều',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Trái đất vốn lạ thường.
+    Mà sao em cứ đi nhầm đường.
+    Lạc vào tim anh lẻ loi.
+    Đằng sau chữ yêu đây là thương.`,
     btnIntro: '^^HiHi^^',
-    title: 'Thúy Kiều mệt mỏi bơ phờ. 🥰',
-    desc: 'Yêu hay không yêu nói một lời!<3 ',
+    title: 'Thúy Kiều mệt mỏi bơ phờ.🥰Nên đành nhẫn nhịn"thôi ơ yêu Anh"',
+    desc: 'Em yêu hay không yêu nói một lời!<3 ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Em có biết vì sao trên tiktok nhiều cô gái xinh, nhưng anh lại chọn em để xem hông?',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3333',
     mess: 'Anh cũng không biết nữa 😘😘',
     messDesc: 'Tối nay nhớ live tiktok 11h nha!.',
     btnAccept: 'Okiiiii lun <3',
