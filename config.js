878b2a91eb2916dd306362198e6ff58b956604c7
thuykieu2k3 <95998563+thuykieu2k3@users.noreply.github.com>
2021-12-12 16:04:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
     titleWeb: "Thúy kiều xinh gái",
     introTitle: 'Thúy Kiều 9 tủi (●'◡'●)',
-    introDesc: `Trái đất vốn lạ thường.
-    Mà sao em cứ đi nhầm đường.
-    Lạc vào tim anh lẻ loi.
-    Đằng sau chữ yêu đây là thương.`,
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^_^',
-    title: 'Phải chăng Anh đã yêu ngay từ cái nhìn đầu tiên😍😍😍.Phải chăng Anh đã say ngay từ lúc thấy nụ cười ấy💕💕💕',
+    title: 'Phải chăng Anh đã yêu ngay từ cái nhìn đầu tiên😍😍😍Phải chăng Anh đã say ngay từ lúc thấy nụ cười ấy💕💕💕',
     desc: 'Yêu hay không yêu nói một lời!<3(e thử ấn trả lời không xem',
     btnYes: '✔Thích lắm😊😊😊 ',
     btnNo: '(❁´◡`❁)Không nha 😜😜😜',
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33',
     mess: 'Anh cũng không biết nữa😅😅😅',
-    messDesc: 'Tối nay nhớ live tiktok 11h nha!.',
+    messDesc: 'Tối nay nhớ live tiktok 11h nha!',
     btnAccept: 'Okiiiii nhoooớ 🤞🤞🤞',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
